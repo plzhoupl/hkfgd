a31df1c323d0ba827958f76303a6d8548bc652d0
chenshengyu <279584274@qq.com>
2017-09-03 11:02:49 +0800

    ele

--- src/components/component/top.vue
+++ src/components/component/top.vue
@@ -19,55 +19,89 @@
     color: #ffffff;
     font-size: 14px;
   }
-  .layout-ceiling a{
-    color: #ffffff;
-  }
 </style>
 <template>
   <div id="top" class="clearfix">
-    <Row>
-    <ul class="layout-ceiling">
-      <Col span="6" offset="4">
-        <li class="left">您好，欢迎光临深圳市亨凯丰精密工业有限公司！</li>
-      </Col>
-      <Col span="6" offset="">
-      <li class="right">联系电话：0755-29722733</li>
-      </Col>
-    </ul>
-    </Row>
-    <Row>
-    <Menu mode="horizontal" theme="primary" active-name="1">
-      <Col span="4" offset="2">
-      <div class="layout-logo">
-        <img src="../../assets/logo.png" alt="">
-      </div>
-      </Col>
-      <Col span="16">
-      <div class="layout-nav">
-        <MenuItem name="1">
-          首页
-        </MenuItem>
-        <MenuItem name="2">
-          关于我们
-        </MenuItem>
-        <MenuItem name="3">
-          产品中心
-        </MenuItem>
-        <MenuItem name="4">
-          设备中心
-        </MenuItem>
-        <MenuItem name="5">
-          人才招聘
-        </MenuItem>
-        <MenuItem name="6">
-          在线留言
-        </MenuItem>
-        <MenuItem name="7">
-          联系我们
-        </MenuItem>
-      </div>
-      </Col>
-    </Menu>
-    </Row>
+    <!--<Row>-->
+    <!--<ul class="layout-ceiling">-->
+      <!--<Col span="6" offset="4">-->
+        <!--<li class="left">您好，欢迎光临深圳市亨凯丰精密工业有限公司！</li>-->
+      <!--</Col>-->
+      <!--<Col span="6" offset="">-->
+      <!--<li class="right">联系电话：0755-29722733</li>-->
+      <!--</Col>-->
+    <!--</ul>-->
+    <!--</Row>-->
+    <!--<Row>-->
+    <!--<Menu mode="horizontal" theme="primary" active-name="1">-->
+      <!--<Col span="4" offset="2">-->
+      <!--<div class="layout-logo">-->
+        <!--<img src="../../assets/logo.png" alt="">-->
+      <!--</div>-->
+      <!--</Col>-->
+      <!--<Col span="16">-->
+      <!--<div class="layout-nav">-->
+        <!--<MenuItem name="1">-->
+          <!--首页-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="2">-->
+          <!--关于我们-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="3">-->
+          <!--产品中心-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="4">-->
+          <!--设备中心-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="5">-->
+          <!--人才招聘-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="6">-->
+          <!--在线留言-->
+        <!--</MenuItem>-->
+        <!--<MenuItem name="7">-->
+          <!--联系我们-->
+        <!--</MenuItem>-->
+      <!--</div>-->
+      <!--</Col>-->
+    <!--</Menu>-->
+    <!--</Row>-->
+    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
 import $ from 'jquery'
 
-Vue.use(iView);
 Vue.config.productionTip = false
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(ElementUI)
+
 require('./main.css')
 /* eslint-disable no-new */
 new Vue({
